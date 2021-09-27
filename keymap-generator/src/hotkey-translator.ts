c2db48a4a1263037a1151e6117c6f6476da0e34d
selimeli <aircondi001a@gmail.com>
2021-09-27 23:55:28 +0800

    init script to generate ahk script from jetbrains keymap xml.

---
+++ keymap-generator/src/hotkey-translator.ts
@@ -0,0 +1,9 @@
+
+export const translateKey = (shortcut: string) => {
+    if (shortcut === 'meta') return '#'
+    if (shortcut === 'control') return '^'
+    if (shortcut === 'shift') return '+'
+    if (shortcut === 'alt') return '!'
+    // TODO: symbols, upper case alphabets, return, delete(backspace), tab
+    return shortcut
+}
