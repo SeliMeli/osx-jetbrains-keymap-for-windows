a5d95fe08fad1211563a2af3e7afaf4b4af2d92f
Puja Jain <pujai@microsoft.com>
2021-10-12 13:44:41 +0530

    added parsing logic

--- keymap-generator/src/main.ts
+++ keymap-generator/src/main.ts
@@ -18,6 +18,27 @@ const keymapConfigReadStream = fs.readFileSync('./resources/osx10_5_plus.xml').t
 
 const parsedKeymap = parser.parse(keymapConfigReadStream, parseOption) as Keymap
 
+var keyAction = [] as any;
+
+var actions = parsedKeymap.keymap.action;
+
+for (var i=0;i<actions.length;i++){
+    var actionName = actions[i]['@_id'];
+    console.log(actionName)
+    var keys = actions[i]["keyboard-shortcut"]
+    if(keys){
+        if(keys.length){ 
+            for (var key of keys) {
+                keyAction.push({"key": key["@_first-keystroke"],"action": actionName})
+            }
+        } else{
+            keyAction.push({"key": keys["@_first-keystroke"],"action": actionName})
+        }
+    }
+}
+
+console.log(keyAction)
+
 //TODO: translate keymap to ahk script.
 
 console.log(JSON.stringify(parsedKeymap.keymap))
