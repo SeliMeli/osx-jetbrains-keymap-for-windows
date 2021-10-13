2fd93199134e8b3982b24cec1a30b8cf25732ddf
Puja Jain <pujai@microsoft.com>
2021-10-13 16:23:10 +0530

    added package alias and argv

--- keymap-generator/src/main.ts
+++ keymap-generator/src/main.ts
@@ -1,6 +1,5 @@
 import * as fs from 'fs';
 import * as parser from "fast-xml-parser";
-import * as readline from 'readline';
 
 export type Shortcut = { '@_first-keystroke': string }
 
@@ -15,38 +14,43 @@ const parseOption = {
     trimValues: true,
 }
 
-let rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+var xmlFilePath = "./resources/osx10_5_plus.xml"
+var executable = "java.exe"
+console.log(process.argv)
+var args = process.argv;
+for (var i=0; i<args.length-1; i++){
+    if(args[i] == '-f' && args[i+1]){
+        xmlFilePath = args[i+1];
+    }
+    if(args[i] == '-ex' && args[i+1]){
+        executable = args[i+1];
+    }
+}
 
-rl.question('Enter xml file to be parsed-', (answer) => {
-    const keymapConfigReadStream = fs.readFileSync(answer).toString()
-    //./resources/osx10_5_plus.xml
-    const parsedKeymap = parser.parse(keymapConfigReadStream, parseOption) as Keymap
+const keymapConfigReadStream = fs.readFileSync(xmlFilePath).toString()
+//./resources/osx10_5_plus.xml
+const parsedKeymap = parser.parse(keymapConfigReadStream, parseOption) as Keymap
 
-    var keyAction = [] as any;
+var keyAction = [] as any;
 
-    var actions = parsedKeymap.keymap.action;
+var actions = parsedKeymap.keymap.action;
 
-    for (var i=0;i<actions.length;i++){
-        var actionName = actions[i]['@_id'];
-        var keys = actions[i]["keyboard-shortcut"]
-        if(keys){
-            if(keys.length){
-                for (var key of keys) {
-                    keyAction.push({"key": key["@_first-keystroke"],"action": actionName})
-                }
-            } else{
-                keyAction.push({"key": keys["@_first-keystroke"],"action": actionName})
+for (var i=0;i<actions.length;i++){
+    var actionName = actions[i]['@_id'];
+    var keys = actions[i]["keyboard-shortcut"]
+    if(keys){
+        if(keys.length){
+            for (var key of keys) {
+                keyAction.push({"key": key["@_first-keystroke"],"action": actionName})
             }
+        } else{
+            keyAction.push({"key": keys["@_first-keystroke"],"action": actionName})
         }
     }
+}
 
-    console.log(keyAction)
+console.log(keyAction)
 
-    //TODO: translate keymap to ahk script.
+//TODO: translate keymap to ahk script.
 
-    console.log(JSON.stringify(parsedKeymap.keymap));
-    rl.close();
-});
+console.log(JSON.stringify(parsedKeymap.keymap));
